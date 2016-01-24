5671f39656ba353fb14c3c0791f6acab7a8e614a
Sushil Khandelwal <sushilforever@gmail.com>
2016-01-24 12:10:29 -0500

    Few inline comments

--- plot1.R
+++ plot1.R
@@ -10,15 +10,18 @@ if(!file.exists("household_power_consumption.zip")) {
   unzip("household_power_consumption.zip")
 }
 
+#Read the file
 power_consumption_ds <- read.table("household_power_consumption.txt", sep = ";", stringsAsFactors = F, header = T, na.strings = c("?"))
 power_consumption_ds$Date <- as.Date(power_consumption_ds$Date, format = "%d/%m/%Y")
 power_consumption_ds$Time <- strptime(power_consumption_ds$Time, format = "%H:%M:%S")
 
+#Subset it for the required date range
 workload_ds <-  subset(power_consumption_ds, Date <= as.Date("2007-02-02"))
 workload_ds <-  subset(workload_ds, Date >= as.Date("2007-02-01"))
 
-
+#Create a file & Set the Device
 png(filename = "plot1.png", width = 480, height = 480)
+#Plot it
 hist(workload_ds$Global_active_power,  
      xlab = "Global Active Power (kilowatts)", 
      main = "Global Active Power",
